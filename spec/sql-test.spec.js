7d774347b2b6605f2c84a398195cfe89a4923bb5
Portia-arch <portia.mkhabela@umuzi.org>
2020-02-07 17:46:43 +0200

    Dammn

--- spec/sql-test.spec.js
+++ spec/sql-test.spec.js
@@ -1,21 +1,39 @@
-const {addNewVisitor} = require('../src/node-script')
+const {addNewVisitor, listVisitors, deleteVisitor} = require('../src/node-script');
 
-describe("node sql", function() {
-    
+     const details = {
+       Name: "Nhlanhla Ngobese",
+       Age: 16,
+       Date: new Date("12/04/2019"),
+       Time: "12:30:00",
+       Assistant: "Lwazi",
+       Comments: "Oh la la"
+     };
+
+describe("node sql", function() {   
 
     it("save data to the database", async function(done) {
 
-        let addthings = await addNewVisitor(2, 'Nhlanhla Ngobese', 16, '12/04/2019', '12:30', 'Lwazi', 'Oh la la')
+        await addNewVisitor(details.Name, details.Age, details.Date, details.Time, details.Assistant, details.Comments)
+            .then(function(data){
 
-        expect(addthings[0].ID).toEqual(2)
-        expect(addthings[0].Visitor_Name).toEqual('Nhlanhla Ngobese')
-        expect(addthings[0].Visitor_Age).toEqual(16)
-        expect(addthings[0].Date_Of_Visit).toEqual('12/04/2019')
-        expect(addthings[0].Time_Of_Visit).toEqual('12:30')
-        expect(addthings[0].Assistant).toEqual('Lwazi')
-        expect(addthings[0].Comments).toEqual('Oh la la')
+                const objDetails = data.rows
 
-        done()
-    })
+                expect(objDetails[0].Name).toEqual(details.Name);
+                expect(objDetails[0].Age).toEqual(details.Age);
+                expect(objDetails[0].Date).toEqual(details.Date);
+                expect(objDetails[0].Time).toEqual(details.Time);
+                expect(objDetails[0].Assistant).toEqual(details.Assistant);
+                expect(objDetails[0].Comments).toEqual(details.Comments);
+        });
 
-})
+        done();
+    });
+
+    xit("should be able to delete a visitor by ID", async function(){
+        expect(await deleteVisitor(id)).toEqual(1)
+    });
+
+    xit("should be able to delete all visitors", async function(){
+        expect(await dropVisitors()).toBeNull()
+    });
+});
--- src/node-script.js
+++ src/node-script.js
@@ -1,137 +1,123 @@
 // require('dotenv').config();
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 const pool = new Pool({
-    user: "user",
-    localhost: "localhost",
-    database: "db",
-    password: "pass",
-    port: 5432
+  user: "user",
+  localhost: "localhost",
+  database: "db",
+  password: "pass",
+  port: 5432
 });
+;
 
-// pool.connect()
+const addNewVisitor = async function(
+  ID,
+  Name,
+  Age,
+  Date,
+  Time,
+  Assistant,
+  Comments
+) {
+  const sql =
+    "INSERT INTO Visitors(Visitor_Name, Visitor_Age, Date_Of_Visit, Time_Of_Visit, Assistant_Name, Comments) VALUES ($1, $2, $3, $4, $5, $6)";
+  const data = [Name, Age, Date, Time, Assistant, Comments];
 
-// Pool.query("Create Table visitors(ID SERIAL PRIMARY KEY, Visitor_Name VARCHAR(100), Visitor_Age INTEGER, Date_Of_Visit DATE, Time_Of_Visit TIME, Assistant_Name VARCHAR(100), Comments VARCHAR(225))", 
-// function(error, results) {
-//     if (error)  {
-//         throw error;
-//     }
-//     console.log(results)
-// });
-
-const addNewVisitor = async (ID, Name, Age, Date, Time, Assistant, Comments) => {
-
-    const sql = 'INSERT INTO Visitors(ID, Visitor_Name, Visitor_Age, Date_Of_Visit, Time_Of_Visit, Assistant_Name, Comments) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-    const data = [ID, Name, Age, Date, Time, Assistant , Comments]
-
-try {
-    results = await pool.query(sql, data)        
-        console.log(results.rows);
-} catch (error) {
-        console.log(error);
-}   
-};
-
-// addNewVisitor(2, 'Nhlanhla Ngobese', 16, '12/04/2019', '12:30', 'Lwazi', 'Oh la la');
-// addNewVisitor(1, 'Busisiwe Mkhabela', 25, '12/06/2019', '09:00', 'Tadiwa', 'Yey!!');
-// addNewVisitor(3, 'Mbali Thusini', 19, '12/01/2019', '09:50', 'Tshepy', 'where');
-// addNewVisitor(5, 'Nqobile Mkhize', 31, '12/12/2019', '08:30', 'Wandile', 'Can I go home already');
-// addNewVisitor(4, 'Sphesihle Buthelezi', 11, '12/08/2019', '10:30', 'Weston', 'Who knew!!');
-
-const listVisitors = async () => {
-
-    const sql = 'Select ID, Visitor_Name FROM Visitors';
-
-try {
-    results = await pool.query(sql)
-      
-        console.log(results.rows);
-        // pool.end();
-} catch (error) {
+  try {
+    results = await pool.query(sql, data);
+    console.log(results.rows);
+  } catch (error) {
     console.log(error);
-}    
-
+  }
 };
 
-// listVisitors();
 
+const listVisitors = async function() {
+  const sql = "Select ID, Visitor_Name FROM Visitors";
 
-const deleteVisitor = async(ID) => {
+  try {
+    results = await pool.query(sql);
 
-    const sql = 'DELETE FROM Visitors WHERE ID= $1';
-    const data = [ID]
+    console.log(results.rows);
 
-try {
-    results = await pool.query(sql, data,)
-        
-        console.log(results.rows);
-        // pool.end();
-
-} catch (error) {
+  } catch (error) {
     console.log(error);
-}    
+  }
 };
 
-// deleteVisitor(1);
 
-const updateVisitor = async (ID, Name, Age, Date, Time, Assistant, Comments) => {
+const deleteVisitor = async function(ID) {
+  const sql = "DELETE FROM Visitors WHERE ID= $1";
+  const data = [ID];
 
-    const sql = 'UPDATE Visitors SET Visitor_Name= $2, Visitor_Age= $3, Date_Of_Visit= $4, Time_Of_Visit= $5, Assistant_Name= $6, Comments= $7 Where ID= $1'
-    const data = [ID, Name, Age, Date, Time, Assistant, Comments]
+  try {
+    results = await pool.query(sql, data);
 
-   try {
-       results = await pool.query(sql, data)
-        //    if (error) {
-        //        throw error
-        //    };
-           console.log(results.rows);
-           // pool.end();
-   } catch (error) {
-       console.log(error);
-   } 
+    console.log(results.rows);
+
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-// updateVisitor(2, 'Yolanda Radebe', 24, '02/03/2017', '12:06', 'Sbonelo', 'Im tired')
 
-const viewVisitor = async(ID) => {
+const updateVisitor = async function(
+  ID,
+  Name,
+  Age,
+  Date,
+  Time,
+  Assistant,
+  Comments
+)  {
+  const sql =
+    "UPDATE Visitors SET Visitor_Name= $2, Visitor_Age= $3, Date_Of_Visit= $4, Time_Of_Visit= $5, Assistant_Name= $6, Comments= $7 Where ID= $1";
+  const data = [ID, Name, Age, Date, Time, Assistant, Comments];
 
-    const sql = 'SELECT * FROM Visitors WHERE ID= $1 ';
-    const data = [ID];
-
-try {
-    results = await pool.query(sql, data)
+  try {
+    results = await pool.query(sql, data);
+   
+    console.log(results.rows);
     
-        console.log(results.rows)
-        // pool.end();
-
-} catch (error) {
+  } catch (error) {
     console.log(error);
-}    
+  }
 };
 
-// viewVisitor(2)
 
-const dropVisitor = async(ID) => {
+const viewVisitor = async function(ID) {
+  const sql = "SELECT * FROM Visitors WHERE ID= $1 ";
+  const data = [ID];
 
-    const sql = 'DELETE FROM Visitors';
-    const data = [ID]
+  try {
+    results = await pool.query(sql, data);
 
-try {
-    results = await pool.query(sql)
-        
-        console.log(results.rows);
-        pool.end();
-
-} catch (error) {
-    console.log(error)
-}    
+    console.log(results.rows);
+ 
+  } catch (error) {
+    console.log(error);
+  }
 };
-// dropVisitor(2, 3, 4, 5)
+
+
+const dropVisitor = async  function() {
+  const sql = "DELETE FROM Visitors";
+
+  try {
+    results = await pool.query(sql);
+
+    console.log(results.rows);
+    // pool.end();
+  } catch (error) {
+    console.log(error);
+  }
+};
+dropVisitor(2, 3, 4, 5)
 
 module.exports = {
-    addNewVisitor,
-    updateVisitor,
-    listVisitors,
-    deleteVisitor,
-    dropVisitor,
-    viewVisitor
-}
+  addNewVisitor,
+  updateVisitor,
+  listVisitors,
+  deleteVisitor,
+  dropVisitor,
+  viewVisitor
+};
